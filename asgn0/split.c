81d3ac4719b16e2dda958c5dee0687227776dd32
alasch <alasch@ucsc.edu>
2022-04-01 20:43:09 +0000

    base functionality

---
+++ asgn0/split.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+int main(int argc, char * argv[]){
+  char inbuff[4096];
+  char delim = argv[1][0];
+  int file = 0;
+  int bytreadcnt = 4096;
+  for(int i = 2; i < argc; i++){
+     if(strcmp(argv[i], "-") == 0){
+       while(bytreadcnt == 4096 ){
+        bytreadcnt = read(0, inbuff, 4096);
+	for(int i = 0; i < bytreadcnt; i++){
+	   if(inbuff[i] == delim){
+	     inbuff[i] = '\n';
+	   }
+	}
+        printf("%d\n", bytreadcnt);
+        write(1, inbuff, bytreadcnt);
+       }
+      
+     }else{
+         file = open(argv[i],O_RDONLY);
+	 while(bytreadcnt == 4096 ){
+ bytreadcnt = read(file , inbuff, 4096);
+
+	for(int i = 0; i < bytreadcnt; i++){
+	   if(inbuff[i] == delim){
+	     inbuff[i] = '\n';
+	   }
+	}
+
+               printf("%d\n", bytreadcnt);
+        write(1, inbuff, bytreadcnt);
+       }
+
+     }
+  }
+}
