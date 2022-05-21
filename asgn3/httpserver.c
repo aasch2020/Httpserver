3f63c03a1b8985c41b65fb018600c750ad765ef8
alasch <alasch@ucsc.edu>
2022-05-21 00:48:25 +0000

    no thread yet

---
+++ asgn3/httpserver.c
@@ -0,0 +1,235 @@
+
+#include <inttypes.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "request.h"
+#include "response.h"
+#include <stdbool.h>
+#define OPTIONS              "t:l:"
+#define BUF_SIZE             4096
+#define DEFAULT_THREAD_COUNT 4
+
+static FILE *logfile;
+#define LOG(...) fprintf(logfile, __VA_ARGS__);
+
+struct Request r;
+
+void sighand() {
+    fflush(logfile);
+    printf("sighanded");
+    fclose(logfile);
+    fflush(stdout);
+    //      request_clear(&r);
+    exit(1);
+}
+
+// Converts a string to an 16 bits unsigned integer.
+// Returns 0 if the string is malformed or out of the range.
+static size_t strtouint16(char number[]) {
+    char *last;
+    long num = strtol(number, &last, 10);
+    if (num <= 0 || num > UINT16_MAX || *last != '\0') {
+        return 0;
+    }
+    return num;
+}
+
+// Creates a socket for listening for connections.
+// Closes the program and prints an error message on error.
+static int create_listen_socket(uint16_t port) {
+    struct sockaddr_in addr;
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        err(EXIT_FAILURE, "socket error");
+    }
+    memset(&addr, 0, sizeof addr);
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htons(INADDR_ANY);
+    addr.sin_port = htons(port);
+    if (bind(listenfd, (struct sockaddr *) &addr, sizeof addr) < 0) {
+        err(EXIT_FAILURE, "bind error");
+    }
+    if (listen(listenfd, 128) < 0) {
+        err(EXIT_FAILURE, "listen error");
+    }
+    return listenfd;
+}
+static void handle_connection(int connfd) {
+    //   logfile = fopen(logfile, "w");
+    int fromend = 0;
+    int altrend = 0;
+    int chekr = 0;
+    char onebuff[2048] = { '\0' };
+    char twobuff[2048] = { '\0' };
+    bool severerr = false;
+    bool badreq = false;
+    // struct Request r;
+    int buffertestfile = open("buffertest.txt", O_RDWR | O_TRUNC);
+    request_init(&r);
+    while (1) {
+        chekr = 0;
+        severerr = false;
+        badreq = false;
+        altrend = 0;
+        fromend = 0;
+        //    badreq = true;
+        //           memset(onebuff, '\0', 2048);
+        //          memset(twobuff, '\0', 2048);
+
+        if (hcreadstart(&r, connfd, fromend, &altrend, onebuff, twobuff) == -1) {
+            severerr = true;
+            break;
+        }
+        if (-1 == headreadstart(&r, connfd, altrend, &fromend, twobuff, onebuff)) {
+            severerr = true;
+            break;
+        }
+        write(buffertestfile, onebuff, 2048);
+        write(buffertestfile, twobuff, 2048);
+        printf("the overwrite into the writing request = %d %d\n", altrend, fromend);
+        int typed = type(&r);
+        switch (typed) {
+        case 1: execute_get(&r, connfd, logfile); break;
+        case 3:
+            if ((chekr = execute_append(&r, connfd, onebuff, &altrend, twobuff, fromend, logfile))
+                == 1) {
+                fromend = 0;
+                altrend = 0;
+                badreq = true;
+                memset(onebuff, '\0', 2048);
+                memset(twobuff, '\0', 2048);
+            }
+
+            break;
+        case 2:
+            if ((chekr = execute_put(&r, connfd, onebuff, &altrend, twobuff, fromend, logfile))
+                == 1) {
+                fromend = 0;
+                altrend = 0;
+                badreq = true;
+                memset(onebuff, '\0', 2048);
+                memset(twobuff, '\0', 2048);
+            }
+            break;
+        case 4:
+            fromend = 0;
+            Response *resp = response_create(400);
+            writeresp(resp, connfd);
+            response_delete(&resp);
+            altrend = 0;
+            badreq = true;
+            memset(onebuff, '\0', 2048);
+            memset(twobuff, '\0', 2048);
+
+            fflush(stdout);
+            break;
+        case 0:
+            fromend = 0;
+            Response *respun = response_create(501);
+            writeresp(respun, connfd);
+            response_delete(&respun);
+
+            altrend = 0;
+            badreq = true;
+            memset(onebuff, '\0', 2048);
+            memset(twobuff, '\0', 2048);
+
+            break;
+        }
+        request_clear(&r);
+        if (chekr == -1) {
+            Response *respun = response_create(500);
+            writeresp(respun, connfd);
+            response_delete(&respun);
+            fromend = 0;
+            altrend = 0;
+            badreq = true;
+            memset(onebuff, '\0', 2048);
+            memset(twobuff, '\0', 2048);
+
+            break;
+        }
+    }
+    close(buffertestfile);
+    (void) connfd;
+}
+
+static void sigterm_handler(int sig) {
+    if (sig == SIGTERM) {
+        warnx("received SIGTERM");
+
+        fclose(logfile);
+
+        fclose(logfile);
+        // request_clear(&r);
+
+        exit(EXIT_SUCCESS);
+    }
+}
+
+static void usage(char *exec) {
+    fprintf(stderr, "usage: %s [-t threads] [-l logfile] <port>\n", exec);
+}
+
+int main(int argc, char *argv[]) {
+    int opt = 0;
+    int threads = DEFAULT_THREAD_COUNT;
+    logfile = stderr;
+
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 't':
+            threads = strtol(optarg, NULL, 10);
+            if (threads <= 0) {
+                errx(EXIT_FAILURE, "bad number of threads");
+            }
+            break;
+        case 'l':
+            logfile = fopen(optarg, "w");
+            if (!logfile) {
+                errx(EXIT_FAILURE, "bad logfile");
+            }
+            break;
+        default: usage(argv[0]); return EXIT_FAILURE;
+        }
+    }
+
+    if (optind >= argc) {
+        warnx("wrong number of arguments");
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    uint16_t port = strtouint16(argv[optind]);
+    if (port == 0) {
+        errx(EXIT_FAILURE, "bad port number: %s", argv[1]);
+    }
+
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, sighand);
+    signal(SIGTERM, sigterm_handler);
+
+    int listenfd = create_listen_socket(port);
+    //    LOG("port=%" PRIu16 ", threads=%d\n", port, threads);
+
+    for (;;) {
+        int connfd = accept(listenfd, NULL, NULL);
+        if (connfd < 0) {
+            warn("accept error");
+            continue;
+        }
+        handle_connection(connfd);
+        close(connfd);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ asgn3/request.c
@@ -0,0 +1,554 @@
+#include "request.h"
+#include <fcntl.h>
+
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "response.h"
+#include <unistd.h>
+#include <fcntl.h>
+void writelog(Request *r, Response *a, FILE *logfile) {
+    fprintf(logfile, "%s,%s,%d,%d\n", get_type(r), get_uri(r), resptype(a), reqid(r));
+    //printf( "%s,%s,%d,%d\n", get_type(r), get_uri(r), resptype(a), reqid(r));
+    fflush(logfile);
+    //  fflush(stdout);
+}
+
+Request *request_create() {
+    Request *r = (Request *) calloc(1, sizeof(Request));
+    r->header_key = (char **) calloc(30, sizeof(char *));
+    r->header_vals = (char **) calloc(30, sizeof(char *));
+    r->numheads = 0;
+    r->content_len = -1;
+    r->numheads = 0;
+    r->badreq = false;
+    r->resptype = 0;
+    r->Reqid = 0;
+    return r;
+}
+
+void request_badflag(Request *r) {
+    r->badreq = true;
+}
+
+int reqid(Request *r) {
+    return r->Reqid;
+}
+void request_update(Request *r, char *match) {
+    //r->header_key = (char **) calloc(30, sizeof(char *));
+    //  r->header_vals = (char **) calloc(30, sizeof(char *));
+
+    sscanf(match, "%8[a-zA-Z] %22s HTTP/%u.%u", r->type, r->uri, &(r->vernum), &(r->verdec));
+}
+void request_init(Request *r) {
+    r->header_key = (char **) calloc(30, sizeof(char *));
+    r->header_vals = (char **) calloc(30, sizeof(char *));
+
+    r->numheads = 0;
+    r->content_len = -1;
+    r->numheads = 0;
+    r->badreq = false;
+    r->resptype = 0;
+    r->Reqid = 0;
+}
+void request_clear(Request *r) {
+
+    r->content_len = -1;
+    //  r->numheads = 0;
+    r->badreq = false;
+    r->resptype = 0;
+    r->Reqid = 0;
+
+    for (int i = 0; i <= r->numheads; i++) {
+        printf("freeing a header\n");
+        free(r->header_key[i]);
+        free(r->header_vals[i]);
+        r->header_key[i] = NULL;
+        r->header_vals[i] = NULL;
+    }
+    r->numheads = 0;
+    free(r->header_key);
+    free(r->header_vals);
+}
+void request_delete(Request **r) {
+    printf("bad delete\n");
+
+    if (*r != NULL) {
+        printf("actually fucking deleting\n");
+        for (int i = 0; i <= ((*r)->numheads); i++) {
+            free((*r)->header_key[i]);
+            free((*r)->header_vals[i]);
+        }
+        //      free((*r)->header_vals);
+        //    free((*r)->header_key);
+        free(*r);
+        *r = NULL;
+    }
+    fflush(stdin);
+}
+int hcreadstart(
+    Request *r, int connfd, int inbufsize, int *fromend, char *inbuffer, char *outbuffer) {
+    int toread = 39;
+    regex_t regm;
+    regex_t done;
+    regmatch_t regs;
+    regmatch_t enma;
+    char readbuff[1024] = { '\0' };
+    strncpy(readbuff, inbuffer, 40);
+    int readed = inbufsize;
+    int readcur = 0;
+    bool found = false;
+    char *statmatch;
+    int lenmatch = 0;
+    int timesgone = 0;
+    while (readed < toread) {
+
+        if (!(inbufsize > 0 && timesgone == 0)) {
+            readcur = read(connfd, readbuff + readed, toread - readed);
+            if (readcur == 0) {
+                return -1;
+            }
+        }
+        regcomp(&regm,
+            "[a-z, A-Z]{1,8}[ ][/][a-zA-Z0-9._/]{1,19}[ ][H][T][T][P][/][0-9][.][0-9][\r][\n]",
+            REG_EXTENDED);
+        regcomp(&done, "[\r][\n]", REG_EXTENDED);
+
+        timesgone++;
+        readed += readcur;
+        if (0 == regexec(&regm, readbuff, 1, &regs, 0)) {
+            lenmatch = regs.rm_eo - regs.rm_so;
+            statmatch = strndup(readbuff + regs.rm_so, lenmatch);
+            request_update(r, statmatch);
+            if (regs.rm_eo != readed) {
+                strncpy(outbuffer, readbuff + regs.rm_eo, readed - regs.rm_eo);
+                *fromend = readed - regs.rm_eo;
+            }
+            regfree(&done);
+            regfree(&regm);
+            free(statmatch);
+            found = true;
+            return 1;
+        }
+        if (0 == regexec(&done, readbuff, 1, &enma, 0)) {
+            r->badreq = true;
+            found = false;
+            if (enma.rm_so != readed) {
+                strncpy(outbuffer, readbuff + enma.rm_eo, readed - enma.rm_eo);
+                *fromend = readed - enma.rm_eo;
+            }
+            regfree(&done);
+            regfree(&regm);
+            return 0;
+        }
+    }
+    r->badreq = true;
+    return 0;
+}
+
+int addheadersfrombuff(Request *r, int inbufsize, int *parsed, char *inbuffer) {
+    regex_t regm;
+    regex_t done;
+    int prevend = 0;
+    if (inbuffer[0] == '\r' && inbuffer[1] == '\n') {
+        *parsed += 2;
+        return 0;
+    }
+    regcomp(&regm, "[!-~]+[:][ ]+[!-~]+[\r][\n]", REG_EXTENDED);
+    regcomp(&done, "[\r][\n][\r][\n]", REG_EXTENDED | REG_NOSUB);
+    regmatch_t regs;
+    char *statmatch;
+    int lenmatch = 0;
+    int trackwhere = 0;
+    while ((regexec(&regm, inbuffer + trackwhere, 1, &regs, 0) == 0) && (trackwhere != inbufsize)) {
+        lenmatch = regs.rm_eo - regs.rm_so;
+        statmatch = strndup(inbuffer + regs.rm_so + trackwhere, lenmatch);
+        add_header(r, statmatch);
+        if ((*(inbuffer + trackwhere + regs.rm_eo)) == '\r'
+            && (*(inbuffer + trackwhere + regs.rm_eo + 1)) == '\n') {
+            *parsed += regs.rm_eo + trackwhere + 2;
+            regfree(&done);
+            regfree(&regm);
+            free(statmatch);
+            return 0;
+        }
+        if (regs.rm_so != 0) {
+            request_badflag(r);
+        }
+        prevend = regs.rm_eo;
+        free(statmatch);
+        trackwhere += regs.rm_eo;
+    }
+    if (regexec(&done, inbuffer + trackwhere, 0, NULL, 0) == 0) {
+        r->badreq = true;
+        regfree(&done);
+        regfree(&regm);
+        return 0;
+    }
+    *parsed += trackwhere;
+    regfree(&done);
+    regfree(&regm);
+    return 1;
+}
+
+int headreadstart(
+    Request *r, int connfd, int inbufsize, int *fromend, char *inbuffer, char *outbuffer) {
+    int toread = 2048;
+    regex_t regm;
+    int parser = 0;
+
+    regcomp(&regm, "[!-~]+[0-9a-zA-Z][:][ ]+[!-~]+[\r][\n]", REG_EXTENDED);
+    char readbuff[2080] = { '\0' };
+    int readed = 0;
+    memcpy(readbuff, inbuffer, inbufsize);
+    readed += inbufsize;
+    int readcur = 0;
+    bool found = false;
+    int timesgone = 0;
+    while (readed < toread || !found) {
+        if (!(inbufsize > 0 && timesgone == 0)) {
+            readcur = read(connfd, readbuff + readed, toread - readed);
+            if (readcur == 0) {
+                regfree(&regm);
+                return -1;
+            }
+        }
+        timesgone++;
+        readed += readcur;
+        int check = addheadersfrombuff(r, readed - parser, &parser, readbuff + parser);
+        if (check == 0) {
+            *fromend = readed - parser;
+            regfree(&regm);
+            memcpy(outbuffer, readbuff + parser, readed - parser);
+            return 1;
+        }
+    }
+    regfree(&regm);
+    return 0;
+}
+
+void add_header(Request *r, char *header_total) {
+    int len = strlen(header_total);
+    char *header_keyin = (char *) calloc(len + 1, sizeof(char));
+    char *header_valin = (char *) calloc(len + 1, sizeof(char));
+    r->numheads++;
+
+    sscanf(header_total, "%[^':']: %[^'\r']", header_keyin, header_valin);
+    r->header_key[r->numheads] = header_keyin;
+    r->header_vals[r->numheads] = header_valin;
+    //free(header_keyin);
+    //free(header_valin);
+    if (strcmp(header_keyin, "Content-Length") == 0) {
+        r->content_len = atoi(header_valin);
+    }
+    if (strcmp(header_keyin, "Request-Id") == 0) {
+        r->Reqid = atoi(header_valin);
+    }
+}
+
+void print_req(Request *r) {
+    for (int i = 0; i < r->numheads; i++) {
+        printf("%s\n", r->header_key[i]);
+        printf("%s\n", r->header_vals[i]);
+    }
+}
+
+int validate(Request *r) {
+    int regs;
+    regex_t reg;
+    regex_t regmethod;
+
+    regcomp(&regmethod, "[A-Za-z]+", REG_EXTENDED);
+    regcomp(&reg, "([/]([a-zA-Z0-9._]+))+", REG_EXTENDED);
+    regs = regexec(&reg, r->uri, 0, NULL, 0);
+    if (regs != 0) {
+        regfree(&regmethod);
+        regfree(&reg);
+
+        return 1;
+    }
+    if (0 != regexec(&regmethod, r->type, 0, NULL, 0)) {
+        regfree(&regmethod);
+        regfree(&reg);
+
+        return 1;
+    }
+
+    if (!(((strcmp(r->type, "GET") == 0) || (strcmp(r->type, "PUT") == 0))
+            || (strcmp(r->type, "APPEND") == 0))) {
+        regfree(&reg);
+        regfree(&reg);
+
+        return 2;
+    }
+    if (!((r->vernum == 1) && (r->verdec == 1))) {
+        regfree(&regmethod);
+        regfree(&reg);
+
+        return 2;
+    }
+    regfree(&reg);
+    regfree(&regmethod);
+
+    return 0;
+}
+
+int type(Request *r) {
+    if (r->vernum != 1 || r->verdec != 1) {
+        return 4;
+    }
+    if (r->badreq) {
+        return 4;
+    }
+
+    if (strcmp(r->type, "GET") == 0) {
+        return 1;
+    }
+    if (strcmp(r->type, "PUT") == 0) {
+        if (r->content_len == -1) {
+            return 4;
+        }
+        return 2;
+    }
+    if (strcmp(r->type, "APPEND") == 0) {
+        if (r->content_len == -1) {
+            return 4;
+        }
+
+        return 3;
+    }
+    return 0;
+}
+
+const char *get_uri(Request *r) {
+    return r->uri;
+}
+const char *get_type(Request *r) {
+    return r->type;
+}
+
+int execute_get(Request *r, int connfd, FILE *logfile) {
+    int types = type(r);
+    if (types == 1) {
+        int opened = open(r->uri + 1, O_RDWR);
+        if (errno == EISDIR) {
+            Response *errrep = response_create(403);
+            writelog(r, errrep, logfile);
+            writeresp(errrep, connfd);
+            response_delete(&errrep);
+            return 1;
+        }
+        //     int error = errno;
+        opened = open(r->uri + 1, O_RDONLY);
+        if (opened == -1) {
+            if ((errno == EACCES) || (errno == EISDIR)) {
+                Response *errrep = response_create(403);
+                writeresp(errrep, connfd);
+                writelog(r, errrep, logfile);
+
+                response_delete(&errrep);
+                return 1;
+            } else if (errno == ENOENT) {
+                Response *errrep = response_create(404);
+                writeresp(errrep, connfd);
+                writelog(r, errrep, logfile);
+
+                response_delete(&errrep);
+
+                return 1;
+            } else {
+                Response *errrep = response_create(500);
+                writeresp(errrep, connfd);
+                writelog(r, errrep, logfile);
+
+                response_delete(&errrep);
+                return 1;
+            }
+        }
+        int resptype = 200;
+        Response *resp = response_create(resptype);
+        write_file(resp, opened, connfd);
+        writelog(r, resp, logfile);
+
+        response_delete(&resp);
+    }
+    return 0;
+}
+int execute_append(Request *r, int connfd, char *buffer, int *fromend, char *writtenfrombuf,
+    int inbufsize, FILE *logfile) {
+    int resptype = 0;
+    int opened = open(r->uri + 1, O_RDWR | O_APPEND);
+    if (opened == -1) {
+        int errord = errno;
+        if (errord == ENOENT) {
+            resptype = 404;
+        } else if ((errord == EACCES) || (errord == EISDIR)) {
+            resptype = 403;
+        } else {
+
+            resptype = 500;
+        }
+    } else {
+        int writed = 0;
+
+        if (inbufsize >= r->content_len) {
+            writed = write(opened, buffer, r->content_len);
+            memcpy(writtenfrombuf, buffer + writed, inbufsize - r->content_len);
+
+            *fromend = inbufsize - r->content_len;
+        } else {
+            int totalwrote = 0;
+            //        int writeloggerput = open("writelogd.txt", O_WRONLY | O_TRUNC);
+
+            if (inbufsize != 0) {
+
+                write(opened, buffer, inbufsize);
+                //        printf("in buf size is %d\n", inbufsize);
+                //          write(writeloggerput, buffer, inbufsize);
+            }
+            ssize_t readed = 0;
+            totalwrote += inbufsize;
+            char bufftwo[2048] = { '\0' };
+            while (totalwrote < r->content_len) {
+                if (r->content_len - totalwrote >= 1024) {
+                    readed = read(connfd, bufftwo, 1024);
+                    totalwrote += readed;
+                    //           printf("cntlen read");
+                    //                           write(writeloggerput, "First\n", 6);
+
+                } else {
+
+                    readed = read(connfd, bufftwo, r->content_len - totalwrote);
+                    //       printf("rembuff read %d\n", r->content_len - totalwrote);
+
+                    totalwrote += readed;
+                }
+                write(opened, bufftwo, readed);
+                //    printf("weird readed nuumber is %zd\n", readed);
+                //      write(writeloggerput, bufftwo, readed);
+
+                if (readed == 0) {
+                    close(opened);
+                    resptype = 501;
+                    return 1;
+                }
+                readed = 0;
+            }
+        }
+        resptype = 200;
+    }
+    Response *resp = response_create(resptype);
+    writeresp(resp, connfd);
+    writelog(r, resp, logfile);
+    close(opened);
+
+    response_delete(&resp);
+    if (resptype != 200) {
+        return 1;
+    }
+    return 0;
+}
+int execute_put(Request *r, int connfd, char *buffer, int *fromend, char *writtenfrombuf,
+    int inbufsize, FILE *logfile) {
+    bool created = false;
+    int resptype = 0;
+    //   printf("put length %d", r->content_len);
+    bool opens = false;
+    int opened = open(r->uri + 1, O_RDWR | O_TRUNC);
+    if (opened == -1) {
+        printf("no file\n");
+        if (errno == ENOENT) {
+            created = false;
+            opens = true;
+            opened = open(r->uri + 1, O_RDWR | O_CREAT, S_IRWXU);
+            if ((errno == EACCES) || (errno == EISDIR)) {
+                resptype = 403;
+                opens = false;
+            } else {
+                created = true;
+            }
+        } else if ((errno == EACCES) || (errno == EISDIR)) {
+            resptype = 403;
+            opens = false;
+        } else {
+            opens = false;
+            resptype = 500;
+        }
+    } else {
+        opens = true;
+    }
+    if (opens) {
+        int writed = 0;
+        if (inbufsize >= r->content_len) {
+            //            printf("printing more than cnt len\n");
+            writed = write(opened, buffer, r->content_len);
+            memcpy(writtenfrombuf, buffer + writed, inbufsize - r->content_len);
+            *fromend = inbufsize - r->content_len;
+        } else {
+            int totalwrote = 0;
+            //          int writeloggerput = open("writelogdput.txt", O_RDWR | O_TRUNC);
+
+            if (inbufsize != 0) {
+                write(opened, buffer, inbufsize);
+                printf("in buf size is %d\n", inbufsize);
+                //          write(writeloggerput, "start\n", 6);
+                //        write(writeloggerput, buffer, inbufsize);
+            }
+
+            ssize_t readed = 0;
+            totalwrote += inbufsize;
+            char bufftwo[2048] = { '\0' };
+
+            while (totalwrote < r->content_len) {
+                if (r->content_len - totalwrote >= 1024) {
+                    printf("cntlen read");
+                    //                            write(writeloggerput, "First\n", 6);
+                    readed = read(connfd, bufftwo, 1024);
+                    totalwrote += readed;
+                } else {
+                    printf("rembuff read %d\n", r->content_len - totalwrote);
+                    readed = read(connfd, bufftwo, r->content_len - totalwrote);
+                    totalwrote += readed;
+                    //                 write(writeloggerput, "second\n", 7);
+                }
+                write(opened, bufftwo, readed);
+                printf("weird readed nuumber is %zd\n", readed);
+                //              write(writeloggerput, bufftwo, readed);
+
+                if (readed == 0) {
+                    close(opened);
+                    //close(writeloggerput);
+                    return -1;
+                }
+                readed = 0;
+            }
+            //close(writeloggerput);
+        }
+        resptype = 200;
+    }
+    close(opened);
+    if (created) {
+
+        Response *resp = response_create(201);
+        writeresp(resp, connfd);
+        writelog(r, resp, logfile);
+
+        response_delete(&resp);
+
+    } else {
+
+        Response *resp = response_create(resptype);
+        writeresp(resp, connfd);
+        writelog(r, resp, logfile);
+
+        response_delete(&resp);
+    }
+    if (resptype != 200) {
+        return 1;
+    }
+    return 0;
+}
